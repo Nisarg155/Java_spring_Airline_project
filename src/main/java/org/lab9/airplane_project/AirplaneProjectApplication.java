fed8ef2149b08e53898b1908c74af562db2c87e6
Nisarg Amlani <114862611+Nisarg155@users.noreply.github.com>
2024-02-20 14:56:08 +0530

    config

---
+++ src/main/java/org/lab9/airplane_project/AirplaneProjectApplication.java
@@ -0,0 +1,13 @@
+package org.lab9.airplane_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirplaneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lab9/airplane_project/AirplaneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lab9.airplane_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
